17e239c75b0f3616400e721bed882f89a93c29bd
Keyhan Vakil <kvakil@berkeley.edu>
2020-07-26 21:12:39 -0700

    fixed tune.sh for BSD readlink

--- tune.sh
+++ tune.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+set -Eeuox pipefail
 
-user_data="$(readlink -f "$1")"
+to_absolute_path() {
+    echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+user_data="$(to_absolute_path "$1")"
 
 output_relative=output
 mkdir -p "$output_relative"
-output_dir="$(readlink -f "$output_relative")" 
+output_dir="$(to_absolute_path "$output_relative")"
 
 docker run -i \
     --mount "type=bind,source=$user_data,target=/data/user" \
     --mount "type=bind,source=$output_dir,target=/model" \
-     kvakil/adapt-lm/train-original
+    docker.pkg.github.com/kvakil/adapt-lm/train-original:latest
