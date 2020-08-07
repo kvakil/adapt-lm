b0b4320fd57a2206cf9304818b1f8b1820f3d16d
Keyhan Vakil <kvakil@berkeley.edu>
2020-08-06 20:16:20 -0700

    Remove unnecessary pre-phrase hook

--- adapt_lm_log.py
+++ adapt_lm_log.py
@@ -4,20 +4,12 @@ from talon_init import TALON_HOME
 
 OUTPUT_FILE = TALON_HOME / "adapt-log"
 
-current_phrase = None
-
-
-def pre_phrase(d):
-    global current_phrase
-    current_phrase = d
-
 
 def post_phrase(d):
     phrase = " ".join(getattr(d["parsed"], "_unmapped", d["phrase"]))
     actions.self.store_phrase(phrase)
 
 
-speech_system.register("pre:phrase", pre_phrase)
 speech_system.register("post:phrase", post_phrase)
 
 mod = Module()
@@ -26,10 +18,10 @@ mod = Module()
 @mod.action_class
 class Actions:
     @staticmethod
-    def store_phrase(words: str):
-        """Record the phrases text to a log for further tuning"""
-        words = words.strip()
-        if not current_phrase or not current_phrase.get("samples") or not words:
+    def store_phrase(phrase: str):
+        """Record the phrase's text to a log for further tuning"""
+        phrase = phrase.strip()
+        if not phrase:
             return
         with OUTPUT_FILE.open("a") as fp:
-            fp.write(f"{words}\n")
+            fp.write(f"{phrase}\n")
